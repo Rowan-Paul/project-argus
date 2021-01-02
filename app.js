229210eaee10f993c5e9b1f5d6c931b7fd17b577
Rowan-Paul <rpflynn80@gmail.com>
2021-01-02 17:11:20 +0100

    Update port console log

--- app.js
+++ app.js
@@ -22,8 +22,6 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
-console.log(process.env.NODE_PORT);
-
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
