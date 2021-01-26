9e1589e02687e3ef865ff97e13c3ab3fa55c7f6e
Rowan-Paul <rpflynn80@gmail.com>
2021-01-26 16:59:15 +0100

    Remove error when an action goes well

--- src/js/redux/account/reducer.js
+++ src/js/redux/account/reducer.js
@@ -9,18 +9,18 @@ const INITIAL_STATE = {
 const reducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case types.REGISTERED:
-      return { ...state, redirect: true };
+      return { ...state, redirect: true, error: "" };
 
     case types.SIGNED_IN:
       localStorage.token = action.payload;
-      return { ...state, redirect: true, token: action.payload };
+      return { ...state, redirect: true, token: action.payload, error: "" };
 
     case types.SIGNED_OUT:
       localStorage.removeItem("token");
-      return { ...state, redirect: true, token: "" };
+      return { ...state, redirect: true, token: "", error: "" };
 
     case types.REDIRECTED:
-      return { ...state, redirect: false };
+      return { ...state, redirect: false, error: "" };
 
     case types.ERROR:
       return { ...state, error: action.payload };
