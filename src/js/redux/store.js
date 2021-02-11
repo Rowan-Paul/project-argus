ed82ff4eca3395c723ae3adfdd7ed5e235a39ae3
Rowan-Paul <rpflynn80@gmail.com>
2021-02-11 17:52:04 +0100

    Fix removing dev tools

--- src/js/redux/store.js
+++ src/js/redux/store.js
@@ -1,11 +1,11 @@
 import { createStore, compose, applyMiddleware } from "redux";
 import rootReducer from "./rootReducer";
 import thunkMiddleware from "redux-thunk";
-const composeEnhancers =
-  (process.env.NODE_ENV === "production" &&
-    typeof window !== "undefined" &&
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
-  compose;
+
+let composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+if (process.env.NODE_ENV === "production") composeEnhancers = compose;
+
 const store = createStore(
   rootReducer,
   composeEnhancers(applyMiddleware(thunkMiddleware))
