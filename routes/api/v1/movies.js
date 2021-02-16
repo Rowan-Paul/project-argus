f5eb26afdc047700863156e3e3f16d3eef497f9e
Rowan-Paul <rpflynn80@gmail.com>
2021-02-16 15:26:44 +0100

    Add POST / route

--- routes/api/v1/movies.js
+++ routes/api/v1/movies.js
@@ -5,8 +5,30 @@ const router = express.Router();
 
 const Movie = require("../../../models/Movie");
 
-router.get("/", (req, res, next) => {
-  res.send("Hello world");
+// Create a movie
+router.post("/", (req, res) => {
+  const { body } = req;
+  const { title } = body;
+  const { year } = body;
+  const { overview } = body;
+
+  if (!title || !year) {
+    return res.status(400).send("Title and year are required");
+  }
+
+  const newMovie = new Movie();
+  newMovie.title = title;
+  newMovie.year = year;
+  if (overview) {
+    newMovie.overview = overview;
+  }
+
+  newMovie.save((err) => {
+    if (err) {
+      return res.status(500).send("Failed to create movie");
+    }
+    return res.sendStatus(201);
+  });
 });
 
 module.exports = router;
