9f2b56caf54a31c66d37230a286d873c790ed357
Rowan-Paul <rpflynn80@gmail.com>
2021-03-08 17:48:37 +0100

    Add margin to h1

--- src/js/dashBoard/DashBoard.js
+++ src/js/dashBoard/DashBoard.js
@@ -23,6 +23,7 @@ function DashboardUI(props) {
     <div className="p-10 pt-20 lg:p-20">
       <h1>Dashboard</h1>
 
+      <h2>Search for a movie</h2>
       <SearchBox />
 
       <div className="grid lg:grid-cols-2 gap-10 mt-5">{moviesArray}</div>
