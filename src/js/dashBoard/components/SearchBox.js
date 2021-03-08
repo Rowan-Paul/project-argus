9495514acbefdf92451224794c3c897fdffbf1dc
Rowan-Paul <rpflynn80@gmail.com>
2021-03-08 16:33:29 +0100

    Add error checking to search

--- src/js/dashBoard/components/SearchBox.js
+++ src/js/dashBoard/components/SearchBox.js
@@ -2,32 +2,48 @@ import { useState } from 'react'
 import { connect } from 'react-redux'
 import { SmallInput } from '../../components/inputFields/SmallInput'
 import { SubmitButton } from '../../components/inputFields/SubmitButton'
+import { setNotice } from '../../redux/main/actions'
 import { searchMovie } from '../../redux/movies/actions'
 
 function SearchBoxUI(props) {
   const [query, setQuery] = useState('')
 
-  return (
-    <div className="flex mt-5">
-      <SmallInput
-        name=""
-        type="text"
-        placeholder="Search for a movie"
-        onChange={(e) => setQuery(e)}
-      />
+  const searchedClicked = (e) => {
+    e.preventDefault()
 
-      <span className="ml-5">
-        <SubmitButton
-          name={'Search'}
-          onClick={() => props.searchMovie(query)}
+    if (document.getElementById('searchForm').checkValidity()) {
+      props.searchMovie(query)
+    } else {
+      console.log('Search field is required')
+
+      props.setNotice({
+        message: 'Search field is required',
+        type: 'error',
+      })
+    }
+  }
+
+  return (
+    <form id="searchForm">
+      <div className="flex mt-5">
+        <SmallInput
+          name=""
+          type="text"
+          placeholder="Search for a movie"
+          onChange={(e) => setQuery(e)}
         />
-      </span>
-    </div>
+
+        <span className="ml-5">
+          <SubmitButton name={'Search'} onClick={(e) => searchedClicked(e)} />
+        </span>
+      </div>
+    </form>
   )
 }
 
 const mapDispatchToProps = (dispatch) => ({
   searchMovie: (query) => dispatch(searchMovie(query)),
+  setNotice: (notice) => dispatch(setNotice(notice)),
 })
 
 export const SearchBox = connect(null, mapDispatchToProps)(SearchBoxUI)
