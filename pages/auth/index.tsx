eeb9fed14d3c8f90ef99ec1f792fd580f0849783
Rowan-Paul <contact@rowanpaulflynn.com>
2021-08-15 18:08:17 +0200

    Update remove double <main>

--- pages/auth/index.tsx
+++ pages/auth/index.tsx
@@ -41,10 +41,7 @@ export default function Auth({ providers }) {
           <link rel="icon" href="/favicon.ico" />
         </Head>
 
-        <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-          <h1>Something went wrong</h1>
-          <p>Try again or contact us.</p>
-        </main>
+        <p>Something went wrong...</p>
       </>
     )
   }
@@ -57,70 +54,68 @@ export default function Auth({ providers }) {
           <link rel="icon" href="/favicon.ico" />
         </Head>
 
-        <main className="flex flex-col items-center justify-center w-full flex-1 md:px-20 text-center">
-          <div className="max-w-sm mx-auto rounded-lg shadow-xl overflow-hidden p-6 space-y-10">
-            <h1>Sign in</h1>
-            {Object.values(providers).map((provider) => {
-              if (provider.id === 'email') {
-                return (
-                  <form
-                    onSubmit={registerUser}
-                    key="emailForm"
-                    className="text-left"
-                    id="emailForm"
-                    noValidate
-                  >
-                    <p className="text-sm text-center pb-2">Or use E-mail</p>
-                    <div className="relative border-b-2 min-w-96 focus-within:border-blue-500">
-                      <input
-                        type="email"
-                        name="email"
-                        placeholder=" "
-                        pattern="^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
-                        required
-                        className="block w-full appearance-none focus:outline-none bg-transparent"
-                      />
-                      <label
-                        htmlFor="email"
-                        className="absolute top-0 -z-1 duration-300 origin-0"
-                      >
-                        E-mail
-                      </label>
-                    </div>
-                    {formError ? (
-                      <p className="text-red-700 text-center w-60 overflow-ellipsis overflow-hidden">
-                        {formError}
-                      </p>
-                    ) : (
-                      <p className="text-red-700 text-center w-60 overflow-ellipsis overflow-hidden"></p>
-                    )}
-                    {formLoading ? (
-                      <span className="p-3 text-white bg-green-400 mt-10 inline-block">
-                        <MaterialIcon request="Pending" /> Loading...
-                      </span>
-                    ) : (
-                      <button
-                        type="submit"
-                        className="mt-3 p-3 text-white bg-green-400"
-                      >
-                        Sign In
-                      </button>
-                    )}
-                  </form>
-                )
-              } else {
-                return (
-                  <span key={provider.name}>
-                    <OnClickButton
-                      text={`${provider.name}`}
-                      onClick={() => signIn(provider.id)}
+        <div className="max-w-sm mx-auto rounded-lg shadow-xl overflow-hidden p-6 space-y-10">
+          <h1>Sign in</h1>
+          {Object.values(providers).map((provider) => {
+            if (provider.id === 'email') {
+              return (
+                <form
+                  onSubmit={registerUser}
+                  key="emailForm"
+                  className="text-left"
+                  id="emailForm"
+                  noValidate
+                >
+                  <p className="text-sm text-center pb-2">Or use E-mail</p>
+                  <div className="relative border-b-2 min-w-96 focus-within:border-blue-500">
+                    <input
+                      type="email"
+                      name="email"
+                      placeholder=" "
+                      pattern="^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
+                      required
+                      className="block w-full appearance-none focus:outline-none bg-transparent"
                     />
-                  </span>
-                )
-              }
-            })}
-          </div>
-        </main>
+                    <label
+                      htmlFor="email"
+                      className="absolute top-0 -z-1 duration-300 origin-0"
+                    >
+                      E-mail
+                    </label>
+                  </div>
+                  {formError ? (
+                    <p className="text-red-700 text-center w-60 overflow-ellipsis overflow-hidden">
+                      {formError}
+                    </p>
+                  ) : (
+                    <p className="text-red-700 text-center w-60 overflow-ellipsis overflow-hidden"></p>
+                  )}
+                  {formLoading ? (
+                    <span className="p-3 text-white bg-green-400 mt-10 inline-block">
+                      <MaterialIcon request="Pending" /> Loading...
+                    </span>
+                  ) : (
+                    <button
+                      type="submit"
+                      className="mt-3 p-3 text-white bg-green-400"
+                    >
+                      Sign In
+                    </button>
+                  )}
+                </form>
+              )
+            } else {
+              return (
+                <span key={provider.name}>
+                  <OnClickButton
+                    text={`${provider.name}`}
+                    onClick={() => signIn(provider.id)}
+                  />
+                </span>
+              )
+            }
+          })}
+        </div>
       </>
     )
   }
