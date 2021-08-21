0cc47ed84ac1ab3d7df57f0d9177c31f41b20fa7
Rowan-Paul <contact@rowanpaulflynn.com>
2021-08-21 20:44:01 +0200

    Remove useless span

--- components/seasons.tsx
+++ components/seasons.tsx
@@ -11,14 +11,12 @@ export default function Seasons({ seasons, tmdb }) {
         <h2>Seasons</h2>
         <div className="py-5 grid gap-4 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
           {Object.values(seasons).map((season: any, i) => (
-            <span>
-              <Item
-                key={season.name}
-                title={season.name}
-                url={`/shows/${router.query.show}/seasons/${season.season_number}`}
-                image={tmdb[i].poster_path}
-              />
-            </span>
+            <Item
+              key={season.name}
+              title={season.name}
+              url={`/shows/${router.query.show}/seasons/${season.season_number}`}
+              image={tmdb[i].poster_path}
+            />
           ))}
         </div>
       </div>
