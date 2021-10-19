7db174e023d04189aef4171d20f337ca9f2e887c
Rowan-Paul <contact@rowanpaulflynn.com>
2021-10-19 20:57:27 +0200

    Add movie & show search

---
+++ pages/api/movies/search.ts
@@ -0,0 +1,36 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { getSession } from 'next-auth/react'
+import { withSentry } from '@sentry/nextjs'
+
+import prisma from '../../../lib/prisma'
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  switch (req.method) {
+    case 'GET':
+      return readMethod(req, res)
+
+    default:
+      res.status(405).end()
+      break
+  }
+}
+
+const readMethod = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const results = await prisma.movies.findMany({
+      where: {
+        title: {
+          contains: req.query.movie as string,
+          mode: 'insensitive',
+        },
+      },
+    })
+
+    if (results.length > 0) res.json(results)
+    else res.status(404).end()
+  } catch (error) {
+    res.status(404).end()
+  }
+}
+
+export default withSentry(handler)
