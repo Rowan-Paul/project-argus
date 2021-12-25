053a4d131b93116e4d0113e8a9ec99a75161ff45
Rowan-Paul <contact@rowanpaulflynn.com>
2021-12-25 14:20:21 +0100

    Remove sentry

--- next.config.js
+++ next.config.js
@@ -3,13 +3,13 @@
 // https://nextjs.org/docs/api-reference/next.config.js/introduction
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-const { withSentryConfig } = require('@sentry/nextjs')
+const { withSentryConfig } = require('@sentry/nextjs');
 
 const moduleExports = {
   images: {
-    domains: ['lh3.googleusercontent.com', 'image.tmdb.org', 'res.cloudinary.com', 'www.themoviedb.org'],
-  },
-}
+    domains: ['lh3.googleusercontent.com', 'image.tmdb.org', 'res.cloudinary.com', 'www.themoviedb.org']
+  }
+};
 
 const SentryWebpackPluginOptions = {
   // Additional config options for the Sentry Webpack plugin. Keep in mind that
@@ -18,11 +18,12 @@ const SentryWebpackPluginOptions = {
   //   release, url, org, project, authToken, configFile, stripPrefix,
   //   urlPrefix, include, ignore
 
-  silent: true, // Suppresses all logs
+  silent: true // Suppresses all logs
   // For all available options, see:
   // https://github.com/getsentry/sentry-webpack-plugin#options.
-}
+};
 
 // Make sure adding Sentry options is the last code to run before exporting, to
 // ensure that your source maps include changes from all other Webpack plugins
-module.exports = withSentryConfig(moduleExports, SentryWebpackPluginOptions)
+// module.exports = withSentryConfig(moduleExports, SentryWebpackPluginOptions)
+moduleExports.exports = moduleExports;
